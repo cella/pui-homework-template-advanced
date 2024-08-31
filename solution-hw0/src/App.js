41ce495466e9036820a65ff744a140092cde51f7
cella <cella.sum@gmail.com>
2024-08-31 18:26:28 -0400

    Update package.json

--- solution-hw0/src/App.js
+++ solution-hw0/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Homework 0 Complete
         </p>
         <a
           className="App-link"
